43850ebc92cfcb83268c08f1130a77fda97f85d8
lilschmitz <KathrinLSchmitz@gmail.com>
2017-02-28 10:16:48 +0000

    Update school_donations.py

--- school_donations.py
+++ school_donations.py
@@ -22,7 +22,7 @@ def index():
 
 @app.route("/donorsUS/projects")
 def donor_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient(MONGODB_URI)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS, limit=55000)
     json_projects = []
